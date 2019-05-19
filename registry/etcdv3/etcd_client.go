0e585c99809fe0cc6d6f42921169b450674fd4a4
zhanghaibin <zhb192729@alibaba-inc.com>
2019-05-19 16:41:05 +0800

    etcd3 registry

--- registry/etcdv3/etcd_client.go
+++ registry/etcdv3/etcd_client.go
@@ -4,7 +4,6 @@ import (
 	log "github.com/AlexStocks/log4go"
 	etcd "github.com/etcd-io/etcd/clientv3"
 	jerrors "github.com/juju/errors"
-	"github.com/samuel/go-zookeeper/zk"
 	"golang.org/x/net/context"
 	"path"
 	"strings"
@@ -65,12 +64,8 @@ func (e *etcdClient) Create(basePath string) error {
 		}
 		e.Unlock()
 		if err != nil {
-			if err == zk.ErrNodeExists {
-				log.Error("zk.create(\"%s\") exists\n", tmpPath)
-			} else {
-				log.Error("zk.create(\"%s\") error(%v)\n", tmpPath, jerrors.ErrorStack(err))
-				return jerrors.Annotatef(err, "zk.Create(path:%s)", basePath)
-			}
+			log.Error("etcd.create(\"%s\") error(%v)\n", tmpPath, jerrors.ErrorStack(err))
+			return jerrors.Annotatef(err, "zk.Create(path:%s)", basePath)
 		}
 	}
 
